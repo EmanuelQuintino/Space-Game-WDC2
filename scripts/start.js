5701b6ef14ae40e4eebc86feb42f3fd01a34e065
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-11-28 21:38:53 -0300

    add spaceship move

--- scripts/start.js
+++ scripts/start.js
@@ -5,6 +5,13 @@ const buttonRank = document.querySelector(".buttonRank");
 function handleSubmitNewGame(event) {
   event.preventDefault();
   localStorage.setItem("@spaceshipGame:playerName", inputName.value);
+  inputName.value = "";
+  window.location.href = "pages/game.html";
+}
+
+function navigateRank() {
+  window.location.href = "pages/rank.html";
 }
 
 newGameForm.addEventListener("submit", handleSubmitNewGame);
+buttonRank.addEventListener("click", navigateRank);
